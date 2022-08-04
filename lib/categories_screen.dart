f05c893affc256973677864c91b14bb8a3d3224f
skyGwork <skyGworkk@gmail.com>
2022-08-04 19:14:46 +0530

    grid view dummy data

---
+++ lib/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCategoryData
+            .map(
+              (catData) => CategoryItem(
+                color: catData.color,
+                title: catData.title,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/grid_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyGridView extends StatelessWidget {
+  const MyGridView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
