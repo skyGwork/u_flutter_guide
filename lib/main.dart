490656d25bf7ccf55cf8456c0ba17040720b117d
skyGwork <skyGworkk@gmail.com>
2022-08-06 09:57:14 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CategoriesScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(
+            // color: Colors.red,
+            // backgroundColor:Colors.red,
+            // color:ColorManager.primary,
+            elevation: 10,
+            centerTitle: true,
+            iconTheme: IconThemeData(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        home: const CategoriesScreen());
   }
 }
